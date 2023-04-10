02672d4935b7d0f6ea32ada65dbeb6adb7df8058
leejieon <42794501+Leejieon@users.noreply.github.com>
2023-04-11 06:51:05 +0900

    Revert "계정 도메인 추가 작업 #17"

--- board-project/src/main/java/com/toy/boardproject/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.toy.boardproject.repository;
-
-import com.toy.boardproject.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
-}
